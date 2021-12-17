3d3fffee88bc7d57b3f35a10abacbac7a586fde3
liufeihe <lfhe2014@163.com>
2021-12-18 01:23:24 +0800

    fix: add feature

---
+++ feature/init_order.go
@@ -0,0 +1,41 @@
+package feature
+
+import "fmt"
+
+var (
+	_  = constInitCheck()
+	v1 = variableInit("v1")
+	v2 = variableInit("v2")
+)
+
+const (
+	c1 = "c1"
+	c2 = "c2"
+)
+
+func constInitCheck() string {
+	if c1 != "" {
+		fmt.Println("const c1 has been initialized.")
+	}
+	if c2 != "" {
+		fmt.Println("const c2 has been initialized.")
+	}
+	return ""
+}
+
+func variableInit(name string) string {
+	fmt.Printf("var %s has been initialzed\n", name)
+	return name
+}
+
+func init() {
+	fmt.Println("first init has been called.")
+}
+
+func init() {
+	fmt.Println("second init has been called.")
+}
+
+func main() {
+	fmt.Println("main.")
+}
