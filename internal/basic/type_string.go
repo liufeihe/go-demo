b1098555c6ebea44b67efc3c6e3fecf998cbf28e
liufeihe <lfhe2014@163.com>
2022-06-18 09:28:53 +0800

    fix: add

--- internal/basic/type_string.go
+++ internal/basic/type_string.go
@@ -5,6 +5,17 @@ import (
 	"unicode/utf8"
 )
 
+func encodeRune(r rune) {
+	buf := make([]byte, 3)
+	_ = utf8.EncodeRune(buf, r)
+	fmt.Printf("Unicode Char: %c, utf8: %x \n", r, buf)
+}
+
+func decodeRune(buf []byte) {
+	r, _ := utf8.DecodeRune(buf)
+	fmt.Printf("utf8: %x, Unicode Char: %s \n", buf, string(r))
+}
+
 func StringDemo() {
 	var c rune = 'a'
 	fmt.Printf("char, %c \n", c)
@@ -16,11 +27,19 @@ func StringDemo() {
 		fmt.Printf("0x%x ", str[i])
 	}
 	fmt.Printf("\n")
+	fmt.Printf("[1]: %x, %c \n", str[1], str[1])
+
 	// 字符视角
 	fmt.Printf("%s, %d\n", str, utf8.RuneCountInString(str))
 	for _, c := range str {
 		fmt.Printf("0x%x", c)
-		fmt.Printf("(%c) ", c)
+		fmt.Printf("(%b)(%c) ", c, c)
 	}
 	fmt.Printf("\n")
+
+	buf := []byte{0xe4, 0xb8, 0xad}
+	decodeRune(buf)
+	encodeRune('a')
+	encodeRune('b')
+	encodeRune('中')
 }
