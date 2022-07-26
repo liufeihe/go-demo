ffffae842561756b3bfa378e4ba1ef1eab89184e
liufeihe <lfhe2014@163.com>
2022-07-26 09:56:45 +0800

    fix: add method

--- internal/basic/type_method.go
+++ internal/basic/type_method.go
@@ -1,3 +1,35 @@
 package basic
 
-func MethodDemo() {}
+import (
+	"fmt"
+	"time"
+)
+
+type T struct{}
+
+func (t T) M(n int) {}
+
+type field struct {
+	name string
+}
+
+func (p field) print() {
+	fmt.Println(p.name)
+}
+
+func MethodDemo() {
+	var t T
+	t.M(1)
+	p := &T{}
+	p.M(2)
+
+	data1 := []*field{{"one"}, {"two"}, {"three"}}
+	for _, v := range data1 {
+		go v.print()
+	}
+	data2 := []field{{"four"}, {"five"}, {"six"}}
+	for _, v := range data2 {
+		go v.print()
+	}
+	time.Sleep(3 * time.Second)
+}
