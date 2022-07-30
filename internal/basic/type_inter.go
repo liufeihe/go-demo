78fb641d46d3ec294ce7950cb9e136e6683b6e86
liufeihe <lfhe2014@163.com>
2022-07-30 11:44:15 +0800

    fix: add

--- internal/basic/type_inter.go
+++ internal/basic/type_inter.go
@@ -1,3 +1,33 @@
 package basic
 
-func InterDemo() {}
+import "fmt"
+
+type MyInterface interface {
+	M1()
+}
+
+type MyT int
+
+func (MyT) M1() {
+	fmt.Println("m1")
+}
+
+func typeCheck() {
+	var t MyT
+	var i interface{} = 1
+	v1, ok := i.(MyInterface)
+	if !ok {
+		fmt.Printf("%T \n", v1)
+	}
+	// v1 = 1
+	var i2 interface{} = t
+	v2, ok := i2.(MyInterface)
+	if ok {
+		fmt.Printf("%T \n", v2)
+		fmt.Println(v2)
+	}
+}
+
+func InterDemo() {
+	typeCheck()
+}
